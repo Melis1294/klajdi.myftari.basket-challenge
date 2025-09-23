1d691e3aae2d58701100c0fe16a368453b585202
KLAJDI MYFTARI <klajdi.myftari94@gmail.com>
2025-09-23 22:42:29 +0200

    [Feature] Separate BallController script into prefab

--- Assets/Code/Scripts/BallController.cs
+++ Assets/Code/Scripts/BallController.cs
@@ -12,11 +12,144 @@ public class BallController : MonoBehaviour
     private string _backboardTag = "Backboard";
     private string _groundTag = "Ground";
 
-    public void ResetState()
+    [SerializeField] private Transform ballStart;
+    [SerializeField] private float _fallSpeed = 1.8f;
+    private float _elapsed = 1.5f;
+    private readonly float _duration = 1.5f; // total time of flight
+    private readonly float _arcHeight = 2f;  // height of the parabola
+    private Vector3 _startPos;
+    private Vector3 _endPos;
+    private Rigidbody _ballRb;
+
+    [SerializeField] float minHoopSpeed = 40;
+    [SerializeField] float maxHoopSpeed = 50;
+    [SerializeField] float minBackboardSpeed = 70;
+    [SerializeField] float maxBackboardSpeed = 75;
+    private float _diversion = 0;
+    private float _shootingSpeed;
+
+    private void Start()
+    {
+        _ballRb = GetComponent<Rigidbody>();
+        ResetState();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ball in the air
+        if (_elapsed < _duration) ComputeFlight();
+    }
+
+    void ComputeFlight()
+    {
+        _elapsed += Time.deltaTime;
+        float t = Mathf.Clamp01(_elapsed / _duration);
+
+        // Linear interpolation in XZ
+        Vector3 horizontalPos = Vector3.Lerp(
+            new Vector3(_startPos.x, 0, _startPos.z),
+            new Vector3(_endPos.x, 0, _endPos.z),
+            t
+        );
+
+        // Parabolic interpolation in Y
+        float y = Mathf.Lerp(_startPos.y, _endPos.y, t) + _arcHeight * 4 * t * (1 - t);
+
+        Vector3 newPos = new Vector3(horizontalPos.x, y, horizontalPos.z);
+
+        // Update rigidbody velocity
+        Vector3 displacement = newPos - _ballRb.position;
+        Vector3 velocity = displacement / Time.deltaTime;
+        _ballRb.velocity = velocity;
+
+        // Rolling rotation
+        Vector3 moveDir = displacement.normalized;
+        Vector3 rollAxis = Vector3.Cross(moveDir, Vector3.up);
+
+        float radius = transform.localScale.x * 0.5f;
+        float speed = displacement.magnitude / Time.deltaTime;
+
+        _ballRb.angularVelocity = -rollAxis * (speed / radius);
+
+        _ballRb.MovePosition(newPos); // Rigid body position update
+
+        // Ball reached the hoop
+        if (_elapsed >= _duration)
+        {
+            // Update physics according to if player is aiming for the hoop or for the backboard
+            _ballRb.velocity = (_shootingSpeed >= minBackboardSpeed) ? (Vector3.forward * _fallSpeed * 0.2f) : (Vector3.down * _fallSpeed);
+            _ballRb.useGravity = true; // Hand control back to physics
+        }
+    }
+
+    // Compute shot logic in relation to right values for hoop and backboard
+    private void UpdateTarget(float shootingSpeed)
+    {
+        _diversion = 0;
+        bool isHoopSpeed = (shootingSpeed >= minHoopSpeed && shootingSpeed <= maxHoopSpeed);
+        bool isBackboardSpeed = (shootingSpeed >= minBackboardSpeed && shootingSpeed <= maxBackboardSpeed);
+
+        if (isBackboardSpeed)
+        {
+            _endPos = GameManager.Instance.Backboard.position;
+        }
+        else if (isHoopSpeed)
+        {
+            _endPos = GameManager.Instance.HoopBasket.position;
+        }
+        else if (shootingSpeed > maxBackboardSpeed)
+        {
+            _endPos = GameManager.Instance.Backboard.position;
+            _diversion = 0.8f;
+        }
+        else
+        {
+            bool isAlmostHoopSpeed = (shootingSpeed > maxHoopSpeed && (shootingSpeed - maxHoopSpeed) <= 5f)
+                || (shootingSpeed < minHoopSpeed && (minHoopSpeed - shootingSpeed) <= 5f);
+            _diversion = isAlmostHoopSpeed ? 0.2f : 0.8f;
+        }
+
+        if (_diversion == 0) return;
+
+        int sign = UnityEngine.Random.value < 0.5f ? -1 : 1;
+        int axis = UnityEngine.Random.value < 0.5f ? -1 : 1;
+        if (axis == -1) _endPos.x += (_diversion * sign);
+        if (axis == 1) _endPos.z += (_diversion * sign);
+    }
+
+    public void Shoot(float shootingSpeed)
+    {
+        _shootingSpeed = shootingSpeed;
+        UpdateTarget(shootingSpeed);
+        _elapsed = 0;
+    }
+
+    private void SetupBallShoot()
     {
         _hoopEntered = false;
         _rimWasTouched = false;
         _backboardWasTouched = false;
+
+        _startPos = ballStart.position;
+        _endPos = GameManager.Instance.HoopBasket.position;
+    }
+
+    public void ResetState()
+    {
+        SetupBallShoot();
+        
+        // Reset ball position w.r.t. player, and physics
+        transform.position = _startPos;
+        transform.LookAt(GameManager.Instance.HoopBasket.transform);
+        ResetPhysics();
+    }
+
+    void ResetPhysics()
+    {
+        _ballRb.useGravity = false;
+        _ballRb.velocity = Vector3.zero;
+        _ballRb.angularVelocity = Vector3.zero;
     }
 
     // Manage collisions with ground, rim and backboard
--- Assets/Code/Scripts/GameManager.cs
+++ Assets/Code/Scripts/GameManager.cs
@@ -10,25 +10,10 @@ public class GameManager : MonoBehaviour
     public Transform ShootingZone;
     [SerializeField] private Transform mainCharacter;
     [SerializeField] int currentPosition = 0;
-    [SerializeField] private float _fallSpeed = 1.8f;
     private Transform _characterInstance;
     private Transform[] _shootingZones;
-    private GameObject _ballInstance;
-    private Vector3 _initialBallLocalPos;
-    private float _elapsed = 1.5f;
-    private readonly float _duration = 1.5f; // total time of flight
-    private readonly float _arcHeight = 2f;  // height of the parabola
-    private Vector3 _startPos;
-    private Vector3 _endPos;
-    private Rigidbody _ballRb;
+    private BallController _ballInstance;
     public static GameManager Instance { get; private set; }
-
-    [SerializeField] float minHoopSpeed = 40;
-    [SerializeField] float maxHoopSpeed = 50;
-    [SerializeField] float minBackboardSpeed = 70;
-    [SerializeField] float maxBackboardSpeed = 75;
-    private float _diversion = 0;
-    private float _shootingSpeed;
 
     public int TotalScore { get;  private set; }
     [SerializeField] private TextMeshProUGUI totalScoreText;
@@ -61,47 +46,16 @@ public class GameManager : MonoBehaviour
         {
             _shootingZones[i] = ShootingZone.GetChild(i);
         }
-    }
 
-    void Start()
-    {
         _characterInstance = Instantiate(mainCharacter, _shootingZones[currentPosition].position, Quaternion.Euler(0, 180f, 0));
-        SpawnBall();
-    }
-
-    void SpawnBall()
-    {
-        //var ballPosition = _shootingZones[currentPosition].position;
-        //ballPosition.y = ballPosition.y + 1f;
-        if (!_ballInstance)
+        if (_characterInstance)
         {
-            _ballInstance = _characterInstance.GetChild(0).gameObject; //Instantiate(ball, ballPosition, Quaternion.identity);
-            _initialBallLocalPos = _ballInstance.transform.localPosition;
-
+            _ballInstance = _characterInstance.GetChild(0).GetComponent<BallController>();
         }
         else
         {
-            _ballInstance.transform.localPosition = _initialBallLocalPos;
-            //_ballInstance.transform.position = ballPosition;
+            throw new NullReferenceException("Character instance not found!");
         }
-
-        // Initialize positions
-        _endPos = HoopBasket.position;
-        _startPos = _ballInstance.transform.position; //ballPosition;
-
-        // Setup ball physics
-        _ballRb = _ballInstance.GetComponent<Rigidbody>();
-        _ballRb.useGravity = false;
-        _ballRb.velocity = Vector3.zero;
-        _ballRb.angularVelocity = Vector3.zero;
-        _ballInstance.transform.LookAt(HoopBasket.transform);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // ball in the air
-        if (_elapsed < _duration) ComputeFlight();
     }
 
     // Called on first shot setup and in next ones
@@ -113,91 +67,11 @@ public class GameManager : MonoBehaviour
         Vector3 direction = HoopBasket.position - _characterInstance.position;
         direction.y = 0;
         _characterInstance.rotation = Quaternion.LookRotation(direction);
-        SpawnBall();
-    }
-
-    void ComputeFlight()
-    {
-        _elapsed += Time.deltaTime;
-        float t = Mathf.Clamp01(_elapsed / _duration);
-
-        // Linear interpolation in XZ
-        Vector3 horizontalPos = Vector3.Lerp(
-            new Vector3(_startPos.x, 0, _startPos.z),
-            new Vector3(_endPos.x, 0, _endPos.z),
-            t
-        );
-
-        // Parabolic interpolation in Y
-        float y = Mathf.Lerp(_startPos.y, _endPos.y, t) + _arcHeight * 4 * t * (1 - t);
-
-        Vector3 newPos = new Vector3(horizontalPos.x, y, horizontalPos.z);
-
-        // Update rigidbody velocity
-        Vector3 displacement = newPos - _ballRb.position;
-        Vector3 velocity = displacement / Time.deltaTime;
-        _ballRb.velocity = velocity;
-
-        // Rolling rotation
-        Vector3 moveDir = displacement.normalized;
-        Vector3 rollAxis = Vector3.Cross(moveDir, Vector3.up);
-
-        float radius = _ballInstance.transform.localScale.x * 0.5f;
-        float speed = displacement.magnitude / Time.deltaTime;
-
-        _ballRb.angularVelocity = -rollAxis * (speed / radius);
-
-        _ballRb.MovePosition(newPos); // Rigid body position update
-
-        // Ball reached the hoop
-        if (_elapsed >= _duration)
-        {
-            // Update physics according to if player is aiming for the hoop or for the backboard
-            _ballRb.velocity = (_shootingSpeed >= minBackboardSpeed) ? (Vector3.forward * _fallSpeed * 0.2f) : (Vector3.down * _fallSpeed);
-            _ballRb.useGravity = true; // Hand control back to physics
-        }
+        //_ballInstance.ResetState();
     }
 
     // Compute shot based on input strength
-    public void OnBallShot(float shootingSpeed)
-    {
-        _shootingSpeed = shootingSpeed;
-        UpdateTarget(shootingSpeed);
-        _elapsed = 0;
-    }
-
-    // Compute shot logic in relation to right values for hoop and backboard
-    private void UpdateTarget(float shootingSpeed)
-    {
-        _diversion = 0;
-        bool isHoopSpeed = (shootingSpeed >= minHoopSpeed && shootingSpeed <= maxHoopSpeed);
-        bool isBackboardSpeed = (shootingSpeed >= minBackboardSpeed && shootingSpeed <= maxBackboardSpeed);
-        
-        if (isBackboardSpeed) 
-        { 
-            _endPos = Backboard.position;
-        } else if (isHoopSpeed)
-        {
-            _endPos = HoopBasket.position;
-        }
-        else if (shootingSpeed > maxBackboardSpeed)
-        {
-            _endPos = Backboard.position;
-            _diversion = 0.8f;
-        } else
-        {
-            bool isAlmostHoopSpeed = (shootingSpeed > maxHoopSpeed && (shootingSpeed - maxHoopSpeed) <= 5f)
-                || (shootingSpeed < minHoopSpeed && (minHoopSpeed - shootingSpeed) <= 5f);
-            _diversion = isAlmostHoopSpeed ? 0.2f : 0.8f;
-        }
-
-        if (_diversion == 0) return;
-
-        int sign = UnityEngine.Random.value < 0.5f ? -1 : 1;
-        int axis = UnityEngine.Random.value < 0.5f ? -1 : 1;
-        if (axis == -1) _endPos.x += (_diversion * sign);
-        if (axis == 1) _endPos.z += (_diversion * sign);
-    }
+    public void OnBallShot(float shootingSpeed) => _ballInstance.Shoot(shootingSpeed);
 
     // Reset game stats for next shot
     public void ResetGameState()
